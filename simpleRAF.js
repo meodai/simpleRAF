ca1b1f286d0dbf4429e342312bc09a910af9fbac
David Aerne <meodai@gmail.com>
2015-05-10 14:07:20 +0200

    adds documentation

--- simpleRAF.js
+++ simpleRAF.js
@@ -14,8 +14,18 @@
       runCallback, removeCallback, loop, startLoop;
 
   callbacks = [];
+  /**
+   * contains all the meta-information for each callback, like current
+   * iteration and iteration
+   */
   callbacksMeta = [];
 
+  /**
+   * removeCallback: removes a callback and its meta data from the corresponding
+   * arrays
+   * @param   {[type]} key function or index in callback array
+   * @returns {[void]}
+   */
   removeCallback = function (key) {
     var i = typeof key === 'function' ? callbacks.indexOf(key) : key;
     if (i > -1) {
@@ -28,6 +38,14 @@
   };
 
   runCallback = function (callback, index, id) {
+  /**
+   * runCallback: calls the individual callbacks and passes the corresponding
+   * meta data as arguments
+   * @param   {Function} callback
+   * @param   {integer}  index      position in callbacks array
+   * @param   {double}   timeStamp  RAF DOMHighResTimeStamp
+   * @returns {void}
+   */
     var returnValue, meta;
 
     meta = callbacksMeta[index];
@@ -40,6 +58,11 @@
   };
 
   loop = function (id) {
+  /**
+   * loop: request animation frame loop
+   * @param   {double} timeStamp RAF DOMHighResTimeStamp
+   * @returns {void}
+   */
     if ( !callbacks.length ) return;
     callbacks.forEach(function(obj, i){
       runCallback(obj, i, id);
@@ -47,6 +70,10 @@
     rafID = window.requestAnimationFrame(loop);
   };
 
+  /**
+   * startLoop kicks of the RAF loop if there is at least one callback
+   * @returns {void}
+   */
   startLoop = function () {
     if (callbacks.length === 1) {
         window.requestAnimationFrame(loop);
@@ -54,12 +81,23 @@
   };
 
   return {
+    /**
+     * on: public method to register a callback
+     * @param   {Function} callback
+     * @param   {float}   increment increment value on every callback
+     * @returns {void}
+     */
     on: function (callback, increment) {
       // todo: add the possibility to pass a increment array
       callbacks.push(callback);
       callbacksMeta.push({val: 0, increment: increment || 1 });
       startLoop();
     },
+    /**
+     * off description]
+     * @param   {Function} callback callback function to remove
+     * @returns {void}
+     */
     off: function (callback) {
       removeCallback(callback);
     }
